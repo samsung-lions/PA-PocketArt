0c0e4cc9f599437e0d6b1c0f2e274070c15d001c
leeejin <asd72621@naver.com>
2024-07-09 15:54:47 +0900

    Gitfix: env파일 수정과 api url,key 숨김

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wixafbbadrjlqppqupbt.supabase.co';
-const supabaseKey =
-  ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpeGFmYmJhZHJqbHFwcHF1cGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0Mjc4MTgsImV4cCI6MjAzNjAwMzgxOH0.CcL7OCTNg6mLPrG8IFEqe7ok3ozKgnX-HCcT4CRxYww';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
