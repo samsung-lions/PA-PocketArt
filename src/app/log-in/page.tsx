e54ae8793d7ecb45420a6e15bf1fbdb030786a06
Leeseayoung <tnfusrltk12@naver.com>
2024-07-12 13:07:19 +0900

    Update: 병합완료

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 
 import supabase from '@/supabase/supabase';
 import { useRouter } from 'next/navigation';
+import { useUserStore } from '@/stores/user';
 
 const LogInPage = () => {
   //login
